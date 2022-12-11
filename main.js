f3a528ee5c30f51508a19df7bc4a2fbe27b3bf4c
endercookie4 <115171932+endercookie4@users.noreply.github.com>
2022-12-11 13:28:23 +0000

    modified:   index.html 	modified:   main.js

--- main.js
+++ main.js
@@ -6,41 +6,31 @@ var milk = 0.25*person
 var nutmeg = 0.25*person
 var butter = 12.5*person
 var bread = 0.5*person
-  function personcounter_plus(){
+  function person_plus(){
     person++
 document.getElementById("person").innerHTML = person
-potato = person*300
-document.getElementById("potato").innerHTML = potato
-salt = person*0.125
-document.getElementById("salt").innerHTML = salt
-milk = person*0.25
-document.getElementById("milk").innerHTML = milk
-nutmeg = person*0.25
-document.getElementById("nutmeg").innerHTML = nutmeg
-butter = person*12.5
-document.getElementById("butter").innerHTML = butter
-bread = person*0.5
-document.getElementById("bread").innerHTML = bread
-
+calc_ingredients()
   }
   
-  function personcounter_minus(){
+  function person_minus(){
     person--
     document.getElementById("person").innerHTML = person
-    potato = person*300
-    document.getElementById("potato").innerHTML = potato
-    salt = person*0.125
-    document.getElementById("salt").innerHTML = salt
-    milk = person*0.25
-    document.getElementById("milk").innerHTML = milk
-    nutmeg = person*0.25
-    document.getElementById("nutmeg").innerHTML = nutmeg
-    butter = person*12.5
-    document.getElementById("butter").innerHTML = butter
-    bread = person*0.5
-    document.getElementById("bread").innerHTML = bread
+    calc_ingredients()
   }
-
+function calc_ingredients(){
+  potato = person*300
+  document.getElementById("potato").innerHTML = potato
+  salt = person*0.125
+  document.getElementById("salt").innerHTML = salt
+  milk = person*0.25
+  document.getElementById("milk").innerHTML = milk
+  nutmeg = person*0.25
+  document.getElementById("nutmeg").innerHTML = nutmeg
+  butter = person*12.5
+  document.getElementById("butter").innerHTML = butter
+  bread = person*0.5
+  document.getElementById("bread").innerHTML = bread
+}
   
 
 
